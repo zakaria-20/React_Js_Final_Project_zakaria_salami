971ef3f1792e4d5ca6976b32a6a358448c082fdc
zakaria-20 <mr.zakariasalami@gmail.com>
2024-09-14 11:48:54 +0100

    edit in app.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// import React from 'react';
-// import { Route, Routes } from "react-router-dom";
-// import Homee from './pages/homee';
-// import Aboutt from './pages/aboutt';
-// import Contacts from './pages/contacts';
-// import Shopp from './pages/shopp';
-// import Detailproducts from './pages/detailproducts';
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Homee from './pages/homee';
+import Aboutt from './pages/aboutt';
+import Contacts from './pages/contacts';
+import Shopp from './pages/shopp';
+import Detailproducts from './pages/detailproducts';
 
-// const App = () => {
-//   return (
-//     <>
-//       <Routes>
+const App = () => {
+  return (
+    <>
+      <Routes>
    
-//           <Route path='/' element={<Homee />} />
-//           <Route path='/about' element={<Aboutt />}/>
-//           <Route path='/contact' element={<Contacts />} />
-//           <Route path='/shop' element={<Shopp />}/>
-//           <Route path='/shop/:id' element={<Detailproducts />}/>
-//       </Routes> 
-//     </>
-//   );
-// }
+          <Route path='/' element={<Homee />} />
+          <Route path='/about' element={<Aboutt />}/>
+          <Route path='/contact' element={<Contacts />} />
+          <Route path='/shop' element={<Shopp />}/>
+          <Route path='/shop/:id' element={<Detailproducts />}/>
+      </Routes> 
+    </>
+  );
+}
 
-// export default App;
+export default App;
