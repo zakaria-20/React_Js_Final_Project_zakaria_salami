4c2cc5ac25225e35cb9439897726cb3b3cdc4c66
zakaria-20 <mr.zakariasalami@gmail.com>
2024-09-14 11:47:28 +0100

    add page shopp

--- src/pages/shopp.jsx
+++ src/pages/shopp.jsx
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import Navbar from '../layouts/navbar';
-// import Section11 from './shop/section11';
-// import Sidebar from './shop/sidebar';
-// import Card from './shop/card';
-// import Footer from '../layouts/footer';
+import React from 'react';
+import Navbar from '../layouts/navbar';
+import Section11 from './shop/section11';
+import Sidebar from './shop/sidebar';
+import Card from './shop/card';
+import Footer from '../layouts/footer';
 
-// const Shopp = () => {
-//     return (
-//         <>
-//             <Navbar />
-//             <Section11 />
-//             <div className='flex pt-20'>
-//                 <div>
-//                     <Sidebar />
-//                 </div>
-//                 <div className='pb-10'>
-//                 <Card />
-//                 </div>
-//             </div>
-//             <Footer />   
-//         </>
-//     );
-// }
+const Shopp = () => {
+    return (
+        <>
+            <Navbar />
+            <Section11 />
+            <div className='flex pt-20'>
+                <div>
+                    <Sidebar />
+                </div>
+                <div className='pb-10'>
+                <Card />
+                </div>
+            </div>
+            <Footer />   
+        </>
+    );
+}
 
-// export default Shopp;
+export default Shopp;
