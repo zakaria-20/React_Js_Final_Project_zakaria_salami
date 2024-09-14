c1ee126b3bb021f49d611c1af7a1e3eb9a0a1477
zakaria-20 <mr.zakariasalami@gmail.com>
2024-09-14 11:44:36 +0100

    add page about

--- src/pages/aboutt.jsx
+++ src/pages/aboutt.jsx
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import Navbar from '../layouts/navbar';
-// import About from './about/about';
-// import Footer from '../layouts/footer';
+import React from 'react';
+import Navbar from '../layouts/navbar';
+import About from './about/about';
+import Footer from '../layouts/footer';
 
-// const Aboutt = () => {
-//     return (
-//         <>
-//             <Navbar />
-//             <About />
-//             <Footer />
-//         </>
-//     );
-// }
+const Aboutt = () => {
+    return (
+        <>
+            <Navbar />
+            <About />
+            <Footer />
+        </>
+    );
+}
 
-// export default Aboutt;
+export default Aboutt;
