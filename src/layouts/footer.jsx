416a664dccfb25374cd3e42fbbe5e673d16d8f0a
zakaria-20 <mr.zakariasalami@gmail.com>
2024-09-14 11:34:10 +0100

    add footer

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -1,67 +1,67 @@
-// import React from 'react';
+import React from 'react';
 
-// const Footer = () => {
-//     return (
-//         <>
-//             <footer class="">
-//   <div class="flex justify-around bg-[#f0f0f0] gap-x-6 pb-12 pt-8">
-//     <div class="max-w-sm">
-//       <div class="mb-6 flex h-12 items-center space-x-2">
-//         <span class="text-xl font-bold">GET IN TOUCH</span>
-//       </div>
-//       <div class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad a officia ea expedita!</div>
-//     </div>
-//     <div class="">
-//       <div class="mt-4 mb-2  xl:mb-4 font-bold">CATEGORIES</div>
-//       <nav aria-label="Footer Navigation" class="text-gray-500">
-//         <ul class="space-y-3">
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Men</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Women</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Dresses</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Sunglasses</a></li>
+const Footer = () => {
+    return (
+        <>
+            <footer class="">
+  <div class="flex justify-around bg-[#f0f0f0] gap-x-6 pb-12 pt-8">
+    <div class="max-w-sm">
+      <div class="mb-6 flex h-12 items-center space-x-2">
+        <span class="text-xl font-bold">GET IN TOUCH</span>
+      </div>
+      <div class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad a officia ea expedita!</div>
+    </div>
+    <div class="">
+      <div class="mt-4 mb-2  xl:mb-4 font-bold">CATEGORIES</div>
+      <nav aria-label="Footer Navigation" class="text-gray-500">
+        <ul class="space-y-3">
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Men</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Women</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Dresses</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Sunglasses</a></li>
          
-//         </ul>
-//       </nav>
-//     </div>
-//     <div class="">
-//       <div class="mt-4 mb-2  xl:mb-4 font-bold">LINKS</div>
-//       <nav aria-label="Footer Navigation" class="text-gray-500">
-//         <ul class="space-y-3">
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Searche</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">About Us</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Contact Us</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Returns</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="">
+      <div class="mt-4 mb-2  xl:mb-4 font-bold">LINKS</div>
+      <nav aria-label="Footer Navigation" class="text-gray-500">
+        <ul class="space-y-3">
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Searche</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">About Us</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Contact Us</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Returns</a></li>
          
-//         </ul>
-//       </nav>
-//     </div>
-//     <div class="">
-//       <div class="mt-4 mb-2  xl:mb-4 font-bold">HELP</div>
-//       <nav aria-label="Footer Navigation" class="text-gray-500">
-//         <ul class="space-y-3">
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Track Order</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Returns</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Shippings</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">Support Hub</a></li>
-//           <li><a class="hover:text-blue-600 hover:underline" href="#">FAQs</a></li>
-//         </ul>
-//       </nav>
-//     </div>
-//     <div class="">
-//       <div class="mt-4 mb-2  xl:mb-4 font-bold">NEWSLETTER</div>
-//       <div class="flex flex-col">
-//         <div class="mb-4">
-//           <input type="email" class="focus:outline mb-2 block h-14 w-full rounded-xl bg-gray-200 px-4 sm:w-80 focus:outline-none focus:ring-1 focus:ring-blue-600" placeholder="Enter your email" />
-//           <button class="block rounded-xl bg-blue-600 px-6 py-3 font-medium text-white">Subscribe</button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
+        </ul>
+      </nav>
+    </div>
+    <div class="">
+      <div class="mt-4 mb-2  xl:mb-4 font-bold">HELP</div>
+      <nav aria-label="Footer Navigation" class="text-gray-500">
+        <ul class="space-y-3">
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Track Order</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Returns</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Shippings</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">Support Hub</a></li>
+          <li><a class="hover:text-blue-600 hover:underline" href="#">FAQs</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="">
+      <div class="mt-4 mb-2  xl:mb-4 font-bold">NEWSLETTER</div>
+      <div class="flex flex-col">
+        <div class="mb-4">
+          <input type="email" class="focus:outline mb-2 block h-14 w-full rounded-xl bg-gray-200 px-4 sm:w-80 focus:outline-none focus:ring-1 focus:ring-blue-600" placeholder="Enter your email" />
+          <button class="block rounded-xl bg-blue-600 px-6 py-3 font-medium text-white">Subscribe</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
-// </footer>
+</footer>
    
-//         </>
-//     );
-// }
+        </>
+    );
+}
 
-// export default Footer;
+export default Footer;
