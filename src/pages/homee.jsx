b48821088c49899c9e015eb103f441adc9dcc056
zakaria-20 <mr.zakariasalami@gmail.com>
2024-09-14 11:46:52 +0100

    add page home

--- src/pages/homee.jsx
+++ src/pages/homee.jsx
@@ -1,24 +1,24 @@
-// import React from 'react';
-// import Navbar from '../layouts/navbar';
-// import Carousel1 from './carousel/carousel1';
-// import Section3 from './secondsection/section3';
-// import Carousel2 from './carousel/carousel2';
-// import Section4 from './secondsection/section4';
-// import Blogs from './blog/blogs';
-// import Footer from '../layouts/footer';
+import React from 'react';
+import Navbar from '../layouts/navbar';
+import Carousel1 from './carousel/carousel1';
+import Section3 from './secondsection/section3';
+import Carousel2 from './carousel/carousel2';
+import Section4 from './secondsection/section4';
+import Blogs from './blog/blogs';
+import Footer from '../layouts/footer';
 
-// const Homee = () => {
-//     return (
-//         <>
-//             <Navbar />
-//             <Carousel1 />
-//             <Section3 />
-//             <Carousel2 />
-//             <Section4 />
-//             <Blogs />
-//             <Footer />
-//         </>
-//     );
-// }
+const Homee = () => {
+    return (
+        <>
+            <Navbar />
+            <Carousel1 />
+            <Section3 />
+            <Carousel2 />
+            <Section4 />
+            <Blogs />
+            <Footer />
+        </>
+    );
+}
 
-// export default Homee;
+export default Homee;
